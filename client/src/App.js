a3f33a6890727d02445b6abf8655d254b947c06a
jacqueshebert <jacques.henri.hebert@gmail.com>
2020-04-25 12:09:21 -0500

    get spotify user data

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { authEndpoint, clientId, redirectUri} from "./config";
+import * as $ from "jquery";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import './App.css';
+import hash from "./hash";
+
+
 
 class App extends Component{
+  constructor() {
+    super();
+    this.state = {}
+  }
+
   componentDidMount() {
     window.fetch('/api/users')
       .then(response => response.json())
       .then(json => console.log(json))
       .catch(error => console.log(error));
+
+      // let _token = hash.access_token;
+    }
+
+    getUser(token) {
+      // Make a call using the token
+      $.ajax({
+        url: "https://api.spotify.com/v1/me/",
+        type: "GET",
+        beforeSend: xhr => {
+          xhr.setRequestHeader("Authorization", "Bearer " + token);
+        },
+        success: data => {
+          console.log(data);
+        }
+      });
     }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+
+      <Router>
+          <div className="App">
+            <header className="App-header">
+              <a
+                  className="btn btn--loginApp-link"
+                  href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`} >
+                  Login to spotify
+                </a>
+                {hash ?  this.getUser(hash.access_token) : console.log("empty")}
+            </header>
+          </div>
+      </Router>
     );
   }
 }
---
+++ client/src/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "4ffe11d5bc884fcdbe93a209e742e51c";
+export const redirectUri = "http://localhost:3000/";
+// export const scopes = [
+//     "user-top-read",
+//     "user-read-currently-playing",
+//     "user-read-playback-state",
+//];
